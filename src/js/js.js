1f89a5ae27c9cbbfae06e1e814ff5f0afa5ecabe
pavlichenko86 <vovan4ik86mail@gmail.com>
2017-12-08 13:27:39 +0200

    Add files via upload

---
+++ src/js/js.js
@@ -0,0 +1,134 @@
+$(document).ready(function() {
+  //TOP MENU
+  $("[data-toggle]").click(function() {
+    var toggle_el = $(this).data("toggle");
+    $(toggle_el).toggleClass("open-sidebar");
+    $(".wrap-main__nav").toggleClass("toLeft");
+    $(".sidebar__link").toggleClass("link-toLeft");
+
+    let screenW = $(window).width();
+    if (screenW >= 580 && screenW < 768) {
+      $(".nav__link").toggleClass("menu__block");
+    }
+  });
+
+  //SHOW PLACES LINKS
+  let hw = $(".house-items").offset().top; //расстояние до верха окна
+  //console.log(hw + "verh okna");
+  let winH = $(window).height(); //высота окна
+  let toEl = hw - winH; //
+
+  $(window).scroll(function() {
+    let winScrollT = $(this).scrollTop(); // высота прокрутки
+    //console.log(hw + "verh okna");
+    //console.log(winScrollT);
+    if (winScrollT > hw - 100) {
+      $(".container-house").css("position", "fixed");
+    }
+    let itH = $(".container-house").height();
+    $(".house-links").css("height", itH * 2);
+    $(".house-empty").css("height", itH);
+    console.log(itH * 2);
+  });
+
+  //BLOCK INFO NAMES
+
+  //my script
+  // let title1 = ['3785 км.', '2 миллиона', 'Пекин']
+  // $('#title2').click(function(){
+  //   let arr = $(this).text();
+  //   console.log(arr);
+  //   let arrStr = arr.split('');
+  //   console.log(arrStr);
+  //   $('.hideInfo').append('<span>arrStr[0]</span>');
+  // });
+
+  // по скролу
+  var target = $(".block__info__title");
+  var targetPos = target.offset().top;
+  var winHeight = $(window).height();
+  var scrollToElem = targetPos - winHeight;
+  $(window).scroll(function() {
+    var winScrollTop = $(this).scrollTop();
+    if (winScrollTop > scrollToElem) {
+      funShow();
+    }
+  });
+
+  function funShow() {
+    var typed = new Typed("#title1", {
+      strings: ["3785 км."],
+      typeSpeed: 50,
+      startDelay: 1000,
+      showCursor: false
+    });
+    var typed = new Typed("#title2", {
+      strings: ["2 миллиона"],
+      typeSpeed: 50,
+      startDelay: 1000,
+      showCursor: false
+    });
+    var typed = new Typed("#title3", {
+      strings: ["Пекин"],
+      typeSpeed: 50,
+      startDelay: 1000,
+      showCursor: false
+    });
+    var typed = new Typed("#title4", {
+      strings: ["379"],
+      typeSpeed: 50,
+      startDelay: 1000,
+      showCursor: false
+    });
+    var typed = new Typed("#title5", {
+      strings: ["25 С<sup>o</sup>"],
+      typeSpeed: 50,
+      startDelay: 1000,
+      showCursor: false,
+      contentType: "html"
+    });
+    var typed = new Typed("#title6", {
+      strings: ["10 000"],
+      typeSpeed: 50,
+      startDelay: 1000,
+      showCursor: false
+    });
+    //$(window).off("scroll");
+  }
+
+  //HOUSE
+
+  let imgW = $(".black__img").css("width");
+  $(".color__img").css("width", imgW);
+
+  $(window).resize(function() {
+    let imgW2 = $(".black__img").css("width");
+    $(".color__img").css("width", imgW2);
+  });
+
+  $(window).scroll(function() {
+    var off = $(".container-house").offset();
+    var barr = off.top; //начало блока
+    var off1 = $(".footer").offset();
+    var barr1 = off1.top; //конец блока
+
+    var top = $(document).scrollTop();
+    if (top > barr && top < barr1) {
+      var hei = $(document).scrollTop();
+      var mmmH = hei - barr; //высота для цветной картики
+      $(".mmm").css("height", mmmH);
+    }
+  });
+
+  //RULES
+
+  let rulH = $(".rules__col").height();
+  let rulR = $(".rules__col-right").height();
+  $(".rules__col-right").css("height", rulH);
+
+  $(window).resize(function() {
+    let rulH = $(".rules__col").height();
+    let rulR = $(".rules__col-right").height();
+    $(".rules__col-right").css("height", rulH);
+  });
+});
